b34f06898e2a4bd9390c6e8e6bd85040391ecfad
ryanaustin45 <55986295+ryanaustin45@users.noreply.github.com>
2022-06-09 21:42:19 +0700

    app.py add description and solution

--- badAI - CC/app.py
+++ badAI - CC/app.py
@@ -53,28 +53,49 @@ def predict():
         result = model.predict(test_image)
         label1 = return_label(result[0])
         prediction = ''
+        description = ''
+        solution = ''
 
         if result[0][label1] < 0.5:
             prediction += 'your pic not scalp'
+            description += 'wrong input image'
+            solution += 'please input scalp image'
         elif label1 == 0:
-            prediction += 'alopecia areata'
+            prediction += 'Alopecia Areata'
+            description += 'Right, it is hair loss and sadly it reaches alopecia areata symptoms. It is an autoimmune disorder that causes your hair to come out. The amount of hair loss is different for everyone. Some people lose it only in a few spots and others sadly lose a lot. Sometimes, hair grows back but falls out again later. In others, hair grows back for good.'
+            solution += 'Find haircare products that focus on hair loss and don\'t forget to see your dermatologist.'
         elif label1 == 1:
-            prediction += 'dandruff'
+            prediction += 'Dandruff'
+            description += 'Those dry, white flakes of skin you brush off your collar or shoulders are called DANDRUFF. It is harmless, but you know it can be embarrassing and itchy. Dandruff isn\'t about your hair, or how often you wash it. Instead, it\'s about the skin on your scalp.'
+            solution += 'Find haircare products that focus on hair loss and don\'t forget to see your dermatologist if you think you need it.'
         elif label1 == 2:
-            prediction += 'folliculitis'
+            prediction += 'Folliculitis'
+            description += 'You got bacteria or a blockage in a tiny pocket in your skin called a hair follicle. Yes, it is folliculitis. Just to add to your knowledge, you have hair follicles just about everywhere you have hair and on the soles of your feet. Folliculitis can make these hair follicles red and swollen.'
+            solution += 'You need to see your dermatologist as soon as possible.'
         elif label1 == 3:
-            prediction += 'Healthy scalp'
+            prediction += 'Healthy Scalp'
+            description += 'Your hair shines brighter than the sun. Congratulations or should I say \'Good Job\' instead?. Keep maintaining your hair using the products that you think that work for your hair.'
+            solution += 'Good Job'
         elif label1 == 4:
-            prediction += 'psoriasis'
+            prediction += 'Psoriasis'
+            description += 'Your skin cells multiply up to 10 times faster than normal and these symptoms are called psoriasis. This makes the skin build up into bumpy red patches covered with white scales. They can grow anywhere, but most appear on the scalp, elbows, knees, and lower back. Psoriasis can\'t be passed from person to person. It does sometimes happen in members of the same family.'
+            solution += 'Moisture your skin and see your dermatologist for better treatment and most importantly to avoid contagion.'
         elif label1 == 5:
-            prediction += 'seborrheic dermatitis'
+            prediction += 'Seborrheic dermatitis'
+            description += 'You got Seborrheic dermatitis! A common skin condition that mainly affects your scalp. It causes scaly patches, red skin, and stubborn dandruff. If you are born with a naturally oily scalp, you are more likely to get this. It might look similar to psoriasis, eczema, or an allergic reaction. It usually happens on your scalp, but you can get it anywhere on your body.'
+            solution += 'You can try looking for over-the-counter dandruff shampoos that contain selenium, zinc pyrithione or coal tar, and shampoo with it twice a week or as directed on the label of the product. If it doesn\'t help, you can go see a dermatologist.'
         elif label1 == 6:
-            prediction += 'tinea capitis'
+            prediction += 'Tinea Capitis'
+            description += 'You got Tinea Capitis, which can also be called Ringworm! It is a rash caused by a fungal infection. It usually causes itchy, scaly, bald patches on the head. Ringworm gets its name because of its circular appearance. Don\'t worry, no worm is involved. But, be careful! It is a contagious infection.'
+            solution += 'Go see a dermatologist as soon as possible for better treatment and most importantly to avoid contagion.'
 
         os.remove("images/{}".format(imagefile.filename))
     else:
         prediction = ''
-    return jsonify(prediction=prediction)
+        description = ''
+        solution = ''
+    return jsonify(prediction=prediction, description=description, solution=solution)
+
 
 
 if __name__ == '__main__':
